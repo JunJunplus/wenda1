aba9682008662460fcf42c44be0a4423395354f1
junjunplus <1966789411@qq.com>
2018-12-27 17:10:56 +0800

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("helloworld");
-        System.out.println("hello world2");
+        System.out.println("hello world");
 
     }
 }
